7ea2ea52bfd4af7a9b5ed53079ae4cb1f48fb67c
rnikiforova <rnikiforova@gmail.com>
2013-04-10 16:54:21 +0300

    Geolocation

---
+++ SliceNDice/HTML5Everywhere/2.Geolocation/geolocation.js
@@ -0,0 +1,49 @@
+﻿function Map(position, options) {
+
+    this.Position = document.getElementById(position);
+    this.Options = options;
+}
+
+Map.prototype.showMap = function () {    
+        
+    var curr = this;
+    
+    if (geoPosition.init()) {
+        geoPosition.getCurrentPosition(setCoords, handleError);
+        window.setTimeout(function () { notNowException(curr.Position) }, 5000);
+    } else {
+        alert("You don't want geolocation? Ok, no problem. I'm cool with that.");
+    }
+
+    function setCoords(position) {
+     
+        var latitude = position.coords.latitude;
+        var longitude = position.coords.longitude;
+
+         var centerPoint = new google.maps.LatLng(latitude, longitude);
+         var showMap = new google.maps.Map(curr.Position, curr.Options);
+         showMap.setCenter(centerPoint);
+    }
+}
+
+function notNowException(position) {
+  
+    var spinner = document.getElementById("loading-spinner");
+
+    if (spinner !== null) {
+        position.innerHTML = "You don't want geolocation? Ok, no problem. I'm cool with that.";
+    }
+}
+
+
+function handleError(err) {
+    alert(JSON.stringify(err));
+}
+
+
+(function Main() {
+    var newMap = new Map("map-div", { zoom: 12, mapTypeId: google.maps.MapTypeId.ROADMAP });
+    newMap.showMap();
+})();
+
+
